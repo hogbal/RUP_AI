7cbacb4cae15de20471abf3792e6ade8a03fe2ef
hogbal <hogbal31@gmail.com>
2021-12-16 14:11:33 +0900

    modify

--- data_aug/app.py
+++ data_aug/app.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from glob import glob
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from object_detection import darknet, darknet_images
@@ -28,13 +29,15 @@ def render_file():
 			while vidcap.isOpened():
 				success, frame = vidcap.read()
 				if success:
-					frame = cv2.imread('test.png')
 					image_resized, image_draw, detections = darknet_images.image_detection(
 							frame, network, class_names, class_colors,.25
 					)
-					image_name = 'save/frame'+str(count)+'.png'
-					cv2.imwrite(image_name,image_resized)
-					darknet_images.save_annotations(image_name, image_draw, detections, class_names)
+					yolo_path = 'static/yolo/'
+					img_name = 'frame'+str(count)+'.png'
+					dis_img_name = 'dis'+str(count)+'.png'
+					cv2.imwrite(yolo_path+dis_img_name, image_draw)
+					cv2.imwrite(yolo_path+img_name, image_resized)
+					darknet_images.save_annotations(yolo_path+img_name, image_draw, detections, class_names)
 				else:
 					break
 				count += 1
@@ -44,6 +47,14 @@ def render_file():
 			return 'upload error'
 
 		return  'detections success'
+
+@app.route('/view')
+def view():
+	filepaths = glob('static/yolo/dis*.png')
+	filename = []
+	for filepath in filepaths:
+		filename.append(os.path.basename(filepath))
+	return render_template("view.html", pics=filename)
 
 @app.route('/')
 def home():
