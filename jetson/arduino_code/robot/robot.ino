bd5e44af540dc4a0d9864f9063b60ef921e1440c
hogbal <hogbal31@gmail.com>
2022-01-21 16:18:13 +0900

    update

--- jetson/arduino_code/robot/robot.ino
+++ jetson/arduino_code/robot/robot.ino
@@ -3,56 +3,110 @@
 
 Adafruit_PWMServoDriver pwm=Adafruit_PWMServoDriver();
 #define SERVOMIN  70 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  540 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMAX  518 // This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define TRIG 9
+#define ECHO 8
+
+int servo_angle0 = 0;
+int servo_angle1 = 165;
+int servo_angle2 = 90;
+int servo_angle3 = 0;
 
 void setup(){
   Serial.begin(9600);
 
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
   pwm.begin();
-  /*
-   * In theory the internal oscillator (clock) is 25MHz but it really isn't
-   * that precise. You can 'calibrate' this by tweaking this number until
-   * you get the PWM update frequency you're expecting!
-   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-   * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
-   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-   *    the I2C PCA9685 chip you are setting the value for.
-   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
-   * Setting the value here is specific to each individual I2C PCA9685 chip and
-   * affects the calculations for the PWM update frequency. 
-   * Failure to correctly set the int.osc value will cause unexpected PWM results
-   */
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   delay(10);
 }
 
+void setARM(int num,int angle){
+  int value = map(angle,0,180,SERVOMIN,SERVOMAX);
+  pwm.setPWM(num,0,value);
+}
+
+void open_func(){
+  setARM(4,0);
+}
+
+void close_func(){
+  setARM(4,50);
+}
+
 void center(){
-  
+  for(;servo_angle1 <= 165 || servo_angle2 >= 90;servo_angle1++,servo_angle2--){
+    if(servo_angle1 <= 165){
+      setARM(1,servo_angle1);
+    }
+    if(servo_angle2 >= 90){
+      setARM(2,servo_angle2);
+    }
+    delay(15);
+  }
+  setARM(3,0);
+  open_func();
+}
+
+void detection(){
+  for(;servo_angle1 >= 135 || servo_angle2 <= 130;servo_angle1--,servo_angle2++){
+    if(servo_angle1 >= 135) {
+      setARM(1,servo_angle1);
+    }
+    if(servo_angle2 <= 130) {
+      setARM(2,servo_angle2);
+    }
+    delay(15);
+  }
+  setARM(3,150);
+  close_func();
 }
 
 void pet(){
-  
-}
-
-void pp(){
-  
-}
-
-void ps(){
-  
+  for(;servo_angle1 >= 135 || servo_angle2 <= 130;servo_angle1--,servo_angle2++){
+    if(servo_angle1 >= 135) {
+      setARM(1,servo_angle1);
+    }
+    if(servo_angle2 <= 130) {
+      setARM(2,servo_angle2);
+    }
+    delay(15);
+  }
+  delay(1000);
+  open_func();
 }
 
 void loop() {
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(0, 0, pulselen);
-    delay(10);
-  }
+  center();
+  delay(1000);
 
-  delay(500);
+  long duration, distance;
+
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn (ECHO, HIGH);
+  distance = duration * 17 / 1000;
+
+  if(distance < 10){
+    close_func();
+    delay(1000);
+    
+    detection();
+    delay(10000);
+
+    center();
+    delay(1000);
+    
+    pet();
+    delay(3000);
+  }
 }
