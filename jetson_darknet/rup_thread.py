d44c53e31ea8575e5b454ce20e96ac0bb7dc3efb
hogbal <hogbal31@gmail.com>
2022-10-15 15:12:31 +0900

    modify jetson

--- jetson_darknet/rup_thread.py
+++ jetson_darknet/rup_thread.py
@@ -3,23 +3,45 @@ import random
 import cv2
 import time
 import darknet
+import serial
+import os
 from threading import Thread, enumerate
 from queue import Queue
 from object_detection import load_camera
 
-'''
 config_file = 'model/yolov7-tiny/yolov7-tiny.cfg'
 data_file = 'model/yolov7-tiny/obj.data'
 weights = 'model/yolov7-tiny/yolov7-tiny_best.weights'
-'''
 
+'''
 config_file = 'model/yolov4-tiny/yolov4-tiny.cfg'
 data_file = 'model/yolov4-tiny/obj.data'
 weights = 'model/yolov4-tiny/yolov4-tiny_best.weights'
-
+'''
 
 thresh = 0.3
 ext_output = False
+
+network, class_names, class_colors = darknet.load_network(
+            config_file,
+            data_file,
+            weights,
+            batch_size=1
+        )
+
+darknet_width = darknet.network_width(network)
+darknet_height = darknet.network_height(network)
+
+#Arduino setting
+ser_main = serial.Serial(
+		port = '/dev/ttyACM0',
+		baudrate=9600)
+
+#csi camera setting
+cap = load_camera.csi_camera()
+
+os.system("clear")
+print("setting success")
 
 def convert2relative(bbox):
     """
@@ -81,16 +103,18 @@ def video_capture(frame_queue, darknet_image_queue):
     cap.release()
 
 
-def inference(darknet_image_queue, detections_queue, fps_queue):
+def inference(darknet_image_queue, detections_queue, fps_queue, label_queue):
     while cap.isOpened():
         darknet_image = darknet_image_queue.get()
         prev_time = time.time()
         detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
+        labels = [detection[0] for detection in detections]
+        if(label_queue.full()):
+            label_queue.get()
+        label_queue.put(labels)
         detections_queue.put(detections)
         fps = int(1/(time.time() - prev_time))
         fps_queue.put(fps)
-        print("FPS: {}".format(fps))
-        darknet.print_detections(detections, ext_output)
         darknet.free_image(darknet_image)
     cap.release()
 
@@ -113,24 +137,48 @@ def drawing(frame_queue, detections_queue, fps_queue):
     cap.release()
     cv2.destroyAllWindows()
 
+def label_check(label_queue):
+    end = time.time() + 20
+    
+    while time.time() < end:
+        labels = label_queue.get()
+        
+        if(len(labels)== 1):
+            return labels[0]
+    return None
+
+def arduino(label_queue):
+    while(True):
+        if(ser_main.readable()):
+            main_res = ser_main.readline()
+            read_data = main_res.decode()[:len(main_res)-1]
+            if(read_data == 'Detection\r'):
+                print("Detection start")
+                label = label_check(label_queue)
+                print("Detection end")
+                
+                if(label == "pet"):
+                    label = '1'
+                elif(label == "pp"):
+                    label = '2'
+                elif(label == "ps"):
+                    label = '3'
+                else:
+                    label = '0'
+                    
+                ser_main.write(label.encode("utf-8"))
+    cap.release()
 
 if __name__ == '__main__':
     frame_queue = Queue()
     darknet_image_queue = Queue(maxsize=1)
     detections_queue = Queue(maxsize=1)
     fps_queue = Queue(maxsize=1)
+    label_queue = Queue(maxsize=1)
 
-    network, class_names, class_colors = darknet.load_network(
-            config_file,
-            data_file,
-            weights,
-            batch_size=1
-        )
-    darknet_width = darknet.network_width(network)
-    darknet_height = darknet.network_height(network)
-    cap = load_camera.csi_camera()
     video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
-    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
+    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue, label_queue)).start()
     Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
+    arduino(label_queue)
